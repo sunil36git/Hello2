735dd467c2c21b4b06a0da5ad0b67d9a019e01b6
sunil36git <46096095+sunil36git@users.noreply.github.com>
2018-12-23 22:48:17 -0600

    Add files via upload

--- src/main/java/com/test/Hello1/web/AppController.java
+++ src/main/java/com/test/Hello1/web/AppController.java
@@ -24,7 +24,7 @@ public class AppController {
 	{
 		logger.info("Request received for name..." + name);
 		logger.info("Response sent for name..." + name);
-		return "Hello 1111111111111111.... "+name;
+		return "Hello 2222222222222.... "+name;
 
 	}
 	
